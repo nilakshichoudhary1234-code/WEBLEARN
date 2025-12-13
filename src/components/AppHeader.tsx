c69bc18e0896147f91c1917836d002e088137cad
Nilakshi Choudhary <nilakshichoudhary1234@gmail.com>
2025-12-13 19:36:56 +0000

    remove this part (_for element p_)

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -39,7 +39,7 @@ export function AppHeader() {
           <DropdownMenuContent className="w-56" align="end" forceMount>
             <DropdownMenuLabel className="font-normal">
               <div className="flex flex-col space-y-1">
-                <p className="text-sm font-medium leading-none">Alex Doe</p>
+                
                 
               </div>
             </DropdownMenuLabel>
